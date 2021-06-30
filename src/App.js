3f8b027c82d5a39f8622273204387ec5faad8d02
Slav <exemple@gmail.com>
2021-06-30 13:18:45 +0300

    Internship-fe-1-3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/';
 import Articles from './Components/Articles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import Artic from './Components/Artic';
-import data from './data/data.json';
+//import data from './data/data.json';
+import firebase from 'firebase';
 
 function App() {
-  const artic = data.map((item) => {
+
+  const [fireCollection, setFireCollection] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection('data').get();
+      setFireCollection(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+    fetchData();
+  }, []);
+
+  const artic = fireCollection.map((item) => {
    return (<Route exact path={`/artic/${item.id}`} key={item.id}>
             <Artic title={item.title} imageUrl={item.imageUrl} description={item.description}/>
           </Route>)
